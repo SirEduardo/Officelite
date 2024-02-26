8ea179b3eb37d1cbc50da5ddddd285bc87660900
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-02-26 14:11:45 +0100

    crowfunding

--- crowdfunding/src/modal.jsx
+++ crowdfunding/src/modal.jsx
@@ -1,46 +1,68 @@
+
+import { useState } from 'react'
 import './modal.css'
 
+export function Modal({closeModal}){
 
-function Modal(){
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+
     return(
-        <div className="container">
-            <header>
+        <div className="modal-container">
+        <div className='modal'>
+        <header className='modal-header'>
+            <nav>
                 <h2>Back this project</h2>
+                <button className='close-modal' onClick={closeModal}>X</button>
+            </nav>
+            <div className='text'>
                 <p>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
-            </header>
-            <div className="no-reward">
-                <input type="checkbox"><label>Pledge with no reward</label></input>
-                <div className="text">
-                    <p>Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.</p> 
-                </div>
             </div>
-            <div className="standard">
-                <input type="checkbox"><label>Bamboo Stand</label></input>
-                <h3>Pledge $25 or more</h3>
-                <p>101 <span className="grey">left</span></p>
-                <div className="text">
-                    <p>You get an ergonomic stand made of natural bamboo. You’ve helped us launch our promotional campaign, and you’ll be added to a special Backer member list.</p>
-                </div>
+        </header>
+        <div className="no-reward">
+            <div className='input'>
+                <label><input type="radio" value='noReward' checked = {selectedOption === 'noReward'} onChange={handleChange} />Pledge with no reward</label>
+            </div>   
+            <div className="text">
+            
+                <p>Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.</p> 
             </div>
-            <div className="black-edition">
-                <input type="checkbox"><label>black Edition Stand</label></input>
-                <h3>Pledge $75 or more</h3>
-                <p>64<span className="grey">left</span></p>
-                <div className="text">
-                    <p>You get a Black Special Edition computer stand and a personal thank you. You’ll be added to out Backer member list. Shipping is included.</p>
-                </div>
-            </div>
-            <div className="special-edition">
-                <input type="checkbox"><label>Mahogany Special Edition</label></input>
-                <h3>Pledge $200 or more</h3>
-                <p>0<span className="grey">left</span></p>
-                <div className="text">
-                    <p>You get two Special Edition Mahogany stands, a Backer T-Shit, and a personal thank you. you’ll be added to our Backer member list. Shippingis included.</p>
-                </div>
-            </div>
-
         </div>
+        <div className="standard">
+            <div className='input'>
+                <label><input type="radio" value='standard' checked = {selectedOption === 'standard'} onChange={handleChange} />Bamboo Stand</label>
+                <h4>Pledge $25 or more</h4>
+                <p>101 <span className="grey">left</span></p>
+            </div>
+            <div className="text">
+                <p>You get an ergonomic stand made of natural bamboo. You’ve helped us launch our promotional campaign, and you’ll be added to a special Backer member list.</p>
+            </div>
+        </div>
+        <div className="modal-black-edition">
+        <div className='input'>
+            <label><input type="radio" value='blackEdition' checked = {selectedOption === 'blackEdition'} onChange={handleChange} />Black Edition Stand</label>
+            <h4>Pledge $75 or more</h4>
+            <p>64 <span className="grey">left</span></p>
+            </div>
+            <div className="text">
+                <p>You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.</p>
+            </div>
+        </div>
+        <div className="modal-special-edition">
+            <div className='input'>
+                <label><input type="radio" value='specialEdition' checked = {selectedOption === 'specialEdition'} onChange={handleChange} />Mahogany Special Edition</label>
+                <h4>Pledge $200 or more</h4>
+                <p>0 <span className="grey">left</span></p>
+            </div>
+            <div className="text">
+                <p>You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.</p>
+            </div>
+        </div>
+        <div className='overlay' onClick={closeModal}></div>
+        </div>
+    </div>
     )
-} 
-
-export default Modal
+}
